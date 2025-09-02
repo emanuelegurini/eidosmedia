c3935ef9f8e6999a4dbe11c434eff999f43e4010
EmanueleGurini <emanuele.gurini89@gmail.com>
2025-09-02 13:41:29 +0200

    feat: add converte from binary to decimal

--- exercise/src/main/java/com/eidosmedia/exercise/Main.java
+++ exercise/src/main/java/com/eidosmedia/exercise/Main.java
@@ -2,6 +2,25 @@ package com.eidosmedia.exercise;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello, World!");
+
+
+        // bit3 * 8 = 1 * 8 = 8
+        // bit2 * 4 = 1 * 4 = 4
+        // bit1 * 2 = 0 * 2 = 0
+        // bit0 * 1 = 1 * 1 = 1
+
+        // 1101 % 10
+
+        int binaryNumber = 1101;
+
+        int bit0 = binaryNumber % 10;
+        int bit1 = (binaryNumber / 10) % 10;
+        int bit2 = (binaryNumber / 100) % 10;
+        int bit3 = (binaryNumber / 100) % 10;
+
+        int decimalNumber = (bit3 * 8) + (bit2 * 4) + (bit1 * 2) + (bit0 * 1);
+
+        System.out.println(decimalNumber);
+
     }
 }
