d98398ea6a3d7648a448941267bb1d04c7b3d6b3
EmanueleGurini <emanuele.gurini89@gmail.com>
2025-09-02 22:46:57 +0200

    Feat: create javalab project that will contain all the java code of the course

---
+++ javalab/src/main/java/com/eidosmedia/javalab/SimpleBinaryConverter.java
@@ -0,0 +1,25 @@
+package com.eidosmedia.javalab;
+
+public class SimpleBinaryConverter {
+
+    public void SympleBinaryConverter() {
+        // bit3 * 8 = 1 * 8 = 8
+        // bit2 * 4 = 1 * 4 = 4
+        // bit1 * 2 = 0 * 2 = 0
+        // bit0 * 1 = 1 * 1 = 1
+
+        // 1101 % 10
+
+        int binaryNumber = 1101;
+
+        int bit0 = binaryNumber % 10;
+        int bit1 = (binaryNumber / 10) % 10;
+        int bit2 = (binaryNumber / 100) % 10;
+        int bit3 = (binaryNumber / 100) % 10;
+
+        int decimalNumber = (bit3 * 8) + (bit2 * 4) + (bit1 * 2) + (bit0 * 1);
+
+        System.out.println(decimalNumber);
+
+    }
+}
