a229ed1075e60f5862c10a351032eabb77d83dff
EmanueleGurini <emanuele.gurini89@gmail.com>
2025-09-03 11:50:48 +0200

    Feat: Add scanner class to get data from the user

--- javalab/src/main/java/com/eidosmedia/javalab/Main.java
+++ javalab/src/main/java/com/eidosmedia/javalab/Main.java
@@ -2,20 +2,31 @@ package com.eidosmedia.javalab;
 
 import java.text.NumberFormat;
 import java.util.Locale;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // let the user insert value inside the program
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insert the name: ");
+        String name = scanner.nextLine().trim();
+
+        System.out.println("Insert the principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.println("Insert the Years of the loan: ");
+        byte year = scanner.nextByte();
+
+        System.out.println("Insert the annual interest: ");
+        byte annualInterest = scanner.nextByte();
 
         // constant
         final byte MONTHS_IN_YEARS = 12;
         final byte PERCENT = 100;
-
-        // inputs
-        int principal = 100000;
-        byte year = 15;
-        byte annualInterest = 6;
 
         // operations
         int numberOfPayments = year * MONTHS_IN_YEARS;
@@ -27,7 +38,7 @@ public class Main {
                         (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
 
         System.out.println(
-                "Monthly Payment : " + NumberFormat
+                "Monthly Payment for" + name + ": " + NumberFormat
                         .getCurrencyInstance(Locale.ITALIAN)
                         .format(monthlyPayment)
         );
